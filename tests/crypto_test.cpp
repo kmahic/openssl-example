2392dab34c40f94c7bb2e3ffaf252c24cc683367
kmahic <kenanmahic@gmail.com>
2017-09-06 21:57:01 +0200

    Removed unnecessary tests

--- tests/crypto_test.cpp
+++ tests/crypto_test.cpp
@@ -13,11 +13,8 @@ int main() {
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 8192, 512 / 8)) == "a941ccbc34d1ee8ebbd1d34824a419c3dc4eac9cbc7c36ae6c7ca8725e2b618a6ad22241e787af937b0960cf85aa8ea3a258f243e05d3cc9b08af5dd93be046c");
   
   assert(Crypto::hex(Crypto::md5("Test")) == "0cbc6611f5540bd0809a388dc95a615b");
-  assert(Crypto::hex(Crypto::md5(Crypto::md5("Test"))) == "5ec20daa6b4e614f2cf221347cf1fe41");
   
   assert(Crypto::hex(Crypto::sha256("Test")) == "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25");
-  assert(Crypto::hex(Crypto::sha256(Crypto::sha256("Test"))) == "28cac1a9a8d521b6aa3b454a19d592e5a113d08a2fbcfefb8a7b977fea140cdd");
   
   assert(Crypto::hex(Crypto::sha512("Test")) == "C6EE9E33CF5C6715A1D148FD73F7318884B41ADCB916021E2BC0E800A5C5DD97F5142178F6AE88C8FDD98E1AFB0CE4C8D2C54B5F37B30B7DA1997BB33B0B8A31");
-  assert(Crypto::hex(Crypto::sha512(Crypto::sha512("Test"))) == "2580131514675B1770772EEE7FFAADE700155F64A03D517AC818753FBC4094829878431AA11929081B208628A61799B6A9F551554AE969DF36703F696C655EFB");
 }
